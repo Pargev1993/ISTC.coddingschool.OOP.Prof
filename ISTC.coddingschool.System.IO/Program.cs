851cea5058352b211aa56c93d0ff8860e04e590e
pqo1993 <pqoalla@gmail.com>
2018-12-20 17:39:23 +0400

    Work with files

--- ISTC.coddingschool.System.IO/Program.cs
+++ ISTC.coddingschool.System.IO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ISTC.coddingschool.System.IO
 {
@@ -10,6 +11,20 @@ namespace ISTC.coddingschool.System.IO
     {
         static void Main(string[] args)
         {
+            var directroty = new DirectoryInfo(@"C:\Windows\assembly");
+            if (directroty.Exists)
+            {
+                Console.WriteLine(directroty.FullName);
+                Console.WriteLine(directroty.Name);
+                Console.WriteLine(directroty.Parent);
+                Console.WriteLine(directroty.Root);
+                Console.WriteLine(directroty.LastAccessTime);
+                Console.WriteLine(directroty.Attributes);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
