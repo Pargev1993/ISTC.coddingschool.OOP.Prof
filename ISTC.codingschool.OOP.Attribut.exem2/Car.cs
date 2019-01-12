6e18487b7a1475f7720dd098bdddfbfedfcb45a1
pqoalla <pqoalla@gmail.com>
2019-01-12 19:50:16 +0400

    Attributes

---
+++ ISTC.codingschool.OOP.Attribut.exem2/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Attribut.exem2
+{
+    [Transport("Porshe","blue",4,speed =280,weight =1500),]
+    class Car : Transport
+    {
+
+    }
+}
