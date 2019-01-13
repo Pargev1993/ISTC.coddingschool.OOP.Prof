6903d6fc175afe9645c6158493105114ff77c90b
pqo1993 <pqoalla@gmail.com>
2019-01-13 21:45:57 +0400

    Attributes

---
+++ ISTC.codingschool.OOP.Attributes.exemple2/Persone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ISTC.codingschool.OOP.Attributes.exemple2
+{
+    class Persone:System.Attribute
+    {
+        [AttributeUsage]
+        [req]
+        public string LastName { get; set; }
+
+
+    }
+}
