640c73b9c5afac82c77f608ac6bdf97183752815
pqo1993 <pqoalla@gmail.com>
2019-01-14 00:18:36 +0400

    Attribute Class Conditional

---
+++ ISTC.codingschool.OOP.Attribute.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApplication1
+{
+    class Motorcycle
+    {
+        [Conditional("Use")]
+        public void UseMotorcycle()
+        {
+            Console.WriteLine("available");
+        }
+
+        [Conditional("NoUse")]
+        public void NoUseMotorcycle()
+        {
+            Console.WriteLine("Not available");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Motorcycle moto = new Motorcycle();
+            moto.UseMotorcycle();
+            moto.NoUseMotorcycle();
+            Console.ReadLine();
+        }
+    }
+}
