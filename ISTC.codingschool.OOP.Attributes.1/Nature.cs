3367f854be78aeda1800b8d0bfd355c6a4c3b6be
pqo1993 <pqoalla@gmail.com>
2019-01-13 23:57:54 +0400

    Attribute exemple 2

---
+++ ISTC.codingschool.OOP.Attributes.1/Nature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ISTC.codingschool.OOP.Attributes._1
+{
+    [AttributeUsage(AttributeTargets.All)]
+    class NatureAttribute:System.Attribute
+    {
+        [JsonProperty("Եխանակ")]
+        public string Season { get; set; }
+        [JsonProperty("Գույն")]
+        public string Color { get; set; }
+        [JsonProperty("Տարի")]
+        public int Year { get; set; }
+        [JsonProperty("Ամիս")]
+        public int Mounth { get; set; }
+        public NatureAttribute(string Season,string Color,int Year,int Mounth)
+        {
+            this.Season = Season;
+            this.Color = Color;
+            this.Year = Year;
+            this.Mounth = Mounth;
+        }
+    }
+}
---
+++ ISTC.codingschool.OOP.Attributes.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Attributes._1
+{
+    [Nature("Winter","White",2019,01)]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAttribute(typeof(Program));
+        }
+        public static void GetAttribute(Type type)
+        {
+            NatureAttribute natureAttribute;
+            natureAttribute = (NatureAttribute)Attribute.GetCustomAttribute(type, typeof(NatureAttribute));
+            Console.WriteLine(natureAttribute.Mounth);
+            Console.WriteLine(natureAttribute.Season);
+            Console.WriteLine(natureAttribute.Year);
+            Console.WriteLine(natureAttribute.Color);
+        }
+    }
+}
