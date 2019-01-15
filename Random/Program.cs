f723550dbbbc4e237879a15517ab9abd611acb2a
pqo1993 <pqoalla@gmail.com>
2019-01-15 13:56:51 +0400

    Out of Memory Exception

---
+++ Random/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace ISTC.ThirdStage.Advance.GC.GC05
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+           
+                Random random = new Random();
+                List<Double> list = new List<double>();
+                int maximum = 900000000;
+                int split = 100000;
+                
+                    for (int count = 1; count <= maximum; count++)
+                    {
+                        list.Add(random.NextDouble());
+                        if (count % split == 0)
+                        {
+                        Console.WriteLine ($"Total item count: {count}.");
+                        }
+                    }
+        }
+    }
+
+}
