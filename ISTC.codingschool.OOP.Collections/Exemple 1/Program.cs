6ecc0d29905da3f44235b4d83cfd9dfd4d873b31
pqo1993 <pqoalla@gmail.com>
2019-01-19 18:22:19 +0400

    Collections

---
+++ ISTC.codingschool.OOP.Collections/Exemple 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(150);
+            list.Add(4);
+            list.Insert(0, 24);
+            list.Insert(0, 25);
+            list.Remove(4);
+            list.Insert(0, 4);
+            var first = list[0];
+            var index = list.IndexOf(24);
+            var contains = list.Contains(4);
+            var contain = list.Contains(4);
+        }
+    }
+}
