fb98e8ceb8d57d4f7d2c65e4e00e20cec1888506
pqo1993 <pqoalla@gmail.com>
2019-01-24 12:54:51 +0400

    Bouble Sort

---
+++ ISTC.codingschool.OOP.Algoritms/BoubleSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoubleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[1000];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 1000);
+            }
+            bool C;
+            do
+            {
+                C = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                        C = true;
+                    }
+                    
+                }
+
+
+            } while (C);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+        public static void Swap(ref int x, ref int y)
+        {
+            int c = x;
+            x = y;
+            y = c;
+        }
+    }
+}
