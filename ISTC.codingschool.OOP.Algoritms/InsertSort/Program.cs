3dbe18cb3b04413c87d73d50eecda37a89b51d83
pqo1993 <pqoalla@gmail.com>
2019-01-25 19:07:13 +0400

    Insert Sort

---
+++ ISTC.codingschool.OOP.Algoritms/InsertSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] =rand.Next(0, 10);
+                //Thread.Sleep(20);
+            }
+
+            for (int j = 1; j <arr.Length; j++)
+            {
+                int T = arr[j];
+                int k = j - 1;
+                while (k>=0&&arr[k]>T)
+                {
+                    arr[k + 1] = arr[k];
+                    k--;
+                }
+                arr[k + 1] = T;
+
+            }
+            for (int r = 0; r < arr.Length; r++)
+            {
+                Console.WriteLine(arr[r]);
+            }
+        }
+        
+    }
+}
