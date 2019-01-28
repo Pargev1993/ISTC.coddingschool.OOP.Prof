a61766e27dcd277dda7de2f8d1f4bce7fc317b0c
pqoalla <pqoalla@gmail.com>
2019-01-28 23:11:41 +0300

    Staff.am About Company

--- ISTC.codingschool.OOP.Staff.am/Staff.am Project/Program.cs
+++ ISTC.codingschool.OOP.Staff.am/Staff.am Project/Program.cs
@@ -18,48 +18,8 @@ namespace Staff.am_Project
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-<<<<<<< HEAD
             string CompanyUrl = "https://staff.am/en/companies";
-          List <Company> companies=  GetContent.GetContentMethod(CompanyUrl);
-            
-=======
-
-            //string Url = "https://staff.am/en/companies?CompaniesFilter%5BkeyWord%5D=&CompaniesFilter%5Bindustries%5D=&CompaniesFilter%5Bindustries%5D%5B%5D=2&CompaniesFilter%5Bemployees_number%5D=&CompaniesFilter%5Bsort_by%5D=&CompaniesFilter%5Bhas_job%5D=";
-            string Url = "https://staff.am/en/jobs";
-            HtmlWeb htmlWeb = new HtmlWeb();
-
-
-            string PatheForChrome = @"C:\Users\Owner\source\Repos\ISTC.coddingschool.System.Prof\ISTC.codingschool.OOP.Staff.am\Staff.am Project\bin\Debug";
-            ChromeOptions co = new ChromeOptions();
-            co.AddArgument("--disable-images");
-            ChromeDriver chromeDriver = new ChromeDriver(PatheForChrome, co);
-
-            chromeDriver.Navigate().GoToUrl(Url);
-            for (int i = 0; i < 40; i++)
-            {
-                try
-                {
-                    chromeDriver.ExecuteScript($"window.scrollBy(0,1750);");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                Thread.Sleep(2000);
-            }
-            var htmldoc = new HtmlDocument();
-            htmldoc.LoadHtml(chromeDriver.PageSource);
-
-            string path = "//div[@class=\"job-inner job-item-title\"]";
-            var nodecollection = htmldoc.DocumentNode.SelectNodes(path);
-            foreach (var item in nodecollection)
-            {
-                Console.WriteLine(item.InnerText);
-                
-                // Console.WriteLine(item.InnerHtml);
-            }
-            Console.WriteLine($"Alsow Find {nodecollection.Count}");
->>>>>>> 69d870d7038a24f63837949bb16e97a0d308bd5e
+            List<Company> companies= GetContent.GetContentMethod(CompanyUrl);
             Console.ReadKey();
         }
     }
