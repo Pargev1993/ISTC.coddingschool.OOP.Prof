69d870d7038a24f63837949bb16e97a0d308bd5e
pqoalla <pqoalla@gmail.com>
2019-01-28 09:43:46 +0400

    Staff.am Jobs

--- ISTC.codingschool.OOP.Staff.am/Staff.am Project/Program.cs
+++ ISTC.codingschool.OOP.Staff.am/Staff.am Project/Program.cs
@@ -17,19 +17,20 @@ namespace Staff.am_Project
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             //string Url = "https://staff.am/en/companies?CompaniesFilter%5BkeyWord%5D=&CompaniesFilter%5Bindustries%5D=&CompaniesFilter%5Bindustries%5D%5B%5D=2&CompaniesFilter%5Bemployees_number%5D=&CompaniesFilter%5Bsort_by%5D=&CompaniesFilter%5Bhas_job%5D=";
             string Url = "https://staff.am/en/jobs";
             HtmlWeb htmlWeb = new HtmlWeb();
 
 
-            string PatheForChrome = @"C:\Users\pqoal\source\repos\ISTC.coddingschool.OOP.Prof\ISTC.codingschool.OOP.Staff.am\Staff.am Project\bin\Debug";
+            string PatheForChrome = @"C:\Users\Owner\source\Repos\ISTC.coddingschool.System.Prof\ISTC.codingschool.OOP.Staff.am\Staff.am Project\bin\Debug";
             ChromeOptions co = new ChromeOptions();
             co.AddArgument("--disable-images");
             ChromeDriver chromeDriver = new ChromeDriver(PatheForChrome, co);
 
             chromeDriver.Navigate().GoToUrl(Url);
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 40; i++)
             {
                 try
                 {
@@ -49,8 +50,10 @@ namespace Staff.am_Project
             foreach (var item in nodecollection)
             {
                 Console.WriteLine(item.InnerText);
+                
                 // Console.WriteLine(item.InnerHtml);
             }
+            Console.WriteLine($"Alsow Find {nodecollection.Count}");
             Console.ReadKey();
         }
     }
