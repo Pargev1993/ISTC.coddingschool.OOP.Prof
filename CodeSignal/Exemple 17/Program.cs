5340cd589e3ffef43232e0aae16d14cfbeede054
pqoalla <pqoalla@gmail.com>
2019-02-12 10:41:26 -0800

    You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input

---
+++ CodeSignal/Exemple 17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_17
+{
+    class Program
+    {public static int arrayChange(int[] inputArray)
+        {
+            int total = 0;
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i - 1] < inputArray[i])
+                    continue;
+
+                total += inputArray[i - 1] + 1 - inputArray[i];
+                inputArray[i] = inputArray[i - 1] + 1;
+            }
+
+            return total;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 3, 5, 5, 5, 4, 12, 12, 10, 15 };
+            arrayChange(array);
+        }
+    }
+}
