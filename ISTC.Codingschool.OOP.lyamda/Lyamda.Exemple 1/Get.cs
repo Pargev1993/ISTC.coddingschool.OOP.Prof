2818812b5b50960deb3ca2e063c408df81f6f416
pqoalla <pqoalla@gmail.com>
2019-02-28 05:27:12 -0800

    Lyamda

---
+++ ISTC.Codingschool.OOP.lyamda/Lyamda.Exemple 1/Get.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyamda.Exemple_1
+{
+   static class Get
+    {
+        public static void getall(int [] x,Func<int,bool> s)
+        {
+
+        }
+    }
+}
--- ISTC.Codingschool.OOP.lyamda/Lyamda.Exemple 1/Program.cs
+++ ISTC.Codingschool.OOP.lyamda/Lyamda.Exemple 1/Program.cs
@@ -8,11 +8,13 @@ namespace Lyamda.Exemple_1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-         
-            
+            int[] array = new int[] { 1, 2, 556, 8, 98 };
 
+            var b = array.Where(d => d % 2 == 0).Where(d => d != 0).ToList();
+  
         }
     }
 }
